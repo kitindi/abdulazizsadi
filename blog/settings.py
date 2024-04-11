40f58edc31d466fcba7f85d4532d97adb86fe921
kitindi <abdullkitindi@gmail.com>
2024-04-11 14:57:24 +0300

    added users app and vectarblog app

--- blog/settings.py
+++ blog/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # local 
+    "main",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.CustomUser"
